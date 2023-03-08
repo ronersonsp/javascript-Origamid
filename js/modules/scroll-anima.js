c62de3f4d0e615cd8759a7164d6c0587f051c699
ronersonsp <duelgd@gmail.com>
2023-03-08 13:32:14 -0300

    Refatoração do scroll-animacao e alterando o nome para scroll-anima

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,26 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowScroll = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - this.windowScroll) < 0;
+      if (isSectionVisible) section.classList.add('ativo');
+      else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    if (this.sections.length) {
+      this.animaScroll();
+      window.addEventListener('scroll', this.animaScroll);
+    }
+  }
+}
